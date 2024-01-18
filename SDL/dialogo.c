2717590e4c5c3a15de467856cfe6607ad672f5f0
leireTo <leire.toledano@alumni.mondragon.edu>
2024-01-18 10:28:40 +0100

    merge

---
+++ SDL/dialogo.c
@@ -0,0 +1,104 @@
+#include "dialogo.h"
+#include "dialogoak.h"
+#include "dialogo_grafiko.h"
+#include "helpers.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <SDL.h>
+
+struct Dialogo* DIALOGO_sortu(char izena[], char dialogoa[], struct Dialogo* aukerak[], int aukerakKop)
+{
+	// struct Dialogo bat malloc gabe sortuko banu, funtzioa amaitzerakoan suntsitua izango litzateke.
+	struct Dialogo* dialogo = malloc(sizeof(*dialogo));
+	ERRORIF(!dialogo, "Memoria gabe geratu gara. =(");
+
+	size_t izenaLen = strlen(izena), dialogoaLen = strlen(dialogoa);
+
+	ERRORIF(aukerakKop > DIALOGO_MAX_AUKERAK, "aukerakKop handiegia pasatu da DIALOGO_sortu funtziora.");
+	ERRORIF(izenaLen > DIALOGO_MAX_STR_LEN, "DIALOGO_sortu funtziora pasatu den izena luzeegia da.");
+	ERRORIF(dialogoaLen > DIALOGO_MAX_STR_LEN, "DIALOGO_sortu funtziora pasatu den dialogoa luzeegia da.");
+
+	dialogo->aukerakKop = aukerakKop;
+
+	memcpy(dialogo->aukerak, aukerak, aukerakKop * sizeof(dialogo));
+	memcpy(dialogo->izena, izena, izenaLen + 1); // memcpy luzeera kalkulatuta dagoelako.
+	memcpy(dialogo->dialogoa, dialogoa, dialogoaLen + 1);
+
+	return dialogo;
+}
+
+void DIALOGO_ezabatu(struct Dialogo* dialogo)
+{
+	int aukera;
+	for (aukera = 0; aukera < dialogo->aukerakKop; ++aukera)
+	{
+		DIALOGO_ezabatu(dialogo->aukerak[aukera]);
+	}
+	free(dialogo);
+}
+
+void DIALOGO_aurrera(struct Dialogo** dialogo, int aukera)
+{
+	ERRORIF(aukera >= (*dialogo)->aukerakKop, "DIALOGO_aurrera funtziora pasatu den aukera handiegia da.");
+
+	*dialogo = (*dialogo)->aukerak[aukera];
+}
+
+void DIALOGO_gauzatu(struct Dialogo* dialogo, enum Animalia animalia, SDL_Renderer* renderer)
+{
+	int aukera;
+
+	while (dialogo->aukerakKop)
+	{
+		aukera = DIALOGO_erakutsi(dialogo, animalia, renderer);
+		DIALOGO_aurrera(&dialogo, aukera);
+	}
+}
+
+void DIALOGO_main(enum Animalia animalia, SDL_Renderer* renderer)
+{
+	struct Dialogo* dialogo = DIALOGO_inizializatu(animalia);
+
+	DIALOGO_gauzatu(dialogo, animalia, renderer);
+
+	DIALOGO_ezabatu(dialogo);
+
+	SELFIE_main(renderer, animalia);
+}
+
+
+struct Dialogo* DIALOGO_inizializatu(enum Animalia animalia)
+{
+	struct Dialogo* dialogo = 0;
+	switch (animalia)
+	{
+	case ANIMALIA_AXOLOTE:
+		DIALOGO_inizializatuAxolote(&dialogo);
+		break;
+	case ANIMALIA_AMAZONIAR_MANATI:
+		DIALOGO_inizializatuAmazoniarManati(&dialogo);
+		break;
+	case ANIMALIA_BELUGA:
+		DIALOGO_inizializatuBeluga(&dialogo);
+		break;
+	case ANIMALIA_BALE_URDIN:
+		DIALOGO_inizializatuBaleUrdin(&dialogo);
+		break;
+	case ANIMALIA_HARTZ_TXURI:
+		DIALOGO_inizializatuHartzTxuri(&dialogo);
+		break;
+	case ANIMALIA_LEMUR:
+		DIALOGO_inizializatuLemur(&dialogo);
+		break;
+	case ANIMALIA_AZERI_ARTIKO:
+		DIALOGO_inizializatuAzeriArtiko(&dialogo);
+		break;
+	case ANIMALIA_JAGUAR:
+		DIALOGO_inizializatuJaguar(&dialogo);
+		break;
+	}
+
+	return dialogo;
+}
---
+++ SDL/helpers.c
@@ -0,0 +1,138 @@
+#include "helpers.h"
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "selfie.h"
+
+// Carga una textura desde un archivo de imagen usando SDL_image.
+SDL_Texture* irudiaKargatuTexturara(SDL_Renderer* renderer, char* path)
+{
+    SDL_Texture* texture;
+    SDL_Surface* surface;
+
+    surface = irudiaKargatuGainazalera(renderer, path);
+    texture = texturaGainazaletik(renderer, surface);
+
+    SDL_FreeSurface(surface);
+    return texture;
+}
+
+SDL_Surface* irudiaKargatuGainazalera(SDL_Renderer* renderer, char* path)
+{
+    SDL_Surface* surface;
+
+    char* fullPath = pathAbsolutua(path);
+
+    surface = IMG_Load(fullPath);
+
+    ERRORIF(!surface, "Error al cargar la imagen %s. SDL_image Error: %s\n", fullPath, IMG_GetError());
+
+    free(fullPath);
+
+    return surface;
+}
+
+
+SDL_Texture* texturaGainazaletik(SDL_Renderer* renderer, SDL_Surface* surface)
+{
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    ERRORIF(!surface, "Error al crear la textura desde la superficie. SDL Error: %s\n", SDL_GetError());
+
+    return texture;
+}
+
+TTF_Font* letraTipoKargatu(int tamaina, char* path)
+{
+    TTF_Font* font;
+
+    char* fullPath = pathAbsolutua(path);
+
+    font = TTF_OpenFont(fullPath, tamaina);
+
+    ERRORIF(!font, "Failed to load font! SDL_ttf Error: %s\n", TTF_GetError());
+
+    free(fullPath);
+
+    return font;
+}
+
+void pantailaGarbitu(SDL_Renderer* renderer)
+{
+    // Marrazteko kolorea beltzera ezarri
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+    // Pantaila marrazteko koloreakin bete
+    SDL_RenderClear(renderer);
+}
+
+// Realiza un efecto de fundido para una textura dada durante un tiempo específico.
+void fadeIn(SDL_Renderer* renderer, SDL_Texture* texture, int tiempo)
+{
+    int startTime = SDL_GetTicks();
+    int currentTime = 0;
+    int alpha = 0;
+
+    while (currentTime < tiempo)
+    {
+        currentTime = SDL_GetTicks() - startTime;
+        alpha = (currentTime * 255) / tiempo;
+        alpha = (alpha > 255) ? 255 : alpha;
+
+        SDL_SetTextureAlphaMod(texture, alpha);
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, alpha);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+
+        pantailaEguneratuEstalkiekin(renderer);
+    }
+}
+
+char* pathAbsolutua(char* path)
+{
+    char* base = SDL_GetBasePath();
+    int baseLen = strlen(base);
+    int pathLen = strlen(path);
+
+    char* fullPath = malloc(baseLen + pathLen + 1);
+
+    ERRORIF(!fullPath, "Memoria gabe geratu gara. =(");
+
+    memcpy(fullPath, base, baseLen);
+    memcpy(fullPath + baseLen, path, pathLen + 1);
+
+    return fullPath;
+}
+
+void handleQuit()
+{
+    SDL_Event e;
+
+    // Deskartatu aurreko ebentu guztiak, SDL_QUIT ezik.
+    while (SDL_PollEvent(&e) != 0)
+    {
+        if (e.type == SDL_QUIT)
+        {
+            fprintf(stderr, "Irtetzen SDL_QUIT jaso delako.");
+            exit(EXIT_SUCCESS);
+        }
+    }
+}
+
+
+void estalkiakMargotu(SDL_Renderer* renderer)
+{
+    SELFIE_goikoSelfieakMarraztu(renderer);
+}
+
+void pantailaEguneratuEstalkiekin(SDL_Renderer* renderer)
+{
+    estalkiakMargotu(renderer);
+    SDL_RenderPresent(renderer);
+}
